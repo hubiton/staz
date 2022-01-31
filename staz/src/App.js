f13c8ab4adb7c51af84f25ec53188fc32103f374
Hubert Jaworski <hubertjaworski14@gmail.com>
2022-01-31 15:40:23 +0100

    dzien1

--- staz/src/App.js
+++ staz/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {Navbar} from './Components/Navbar/Navbar'
+import {Header} from './Components/Header/Header'
+import * as ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
 
 function App() {
+  const Imie = 'Imie';
+  const Nazwisko = 'Nazwisko'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
+        <Navbar/>
     </div>
   );
 }
+
 
 export default App;
