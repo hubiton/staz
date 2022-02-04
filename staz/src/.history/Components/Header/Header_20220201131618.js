80ac2dd0a59ec1a872ba83253a6d6101b596098f
Hubert Jaworski <hubertjaworski14@gmail.com>
2022-02-04 13:06:05 +0100

    fix v1

--- staz/src/.history/Components/Header/Header_20220201131618.js
+++ staz/src/.history/Components/Header/Header_20220201131618.js
@@ -5,7 +5,7 @@ export const Header = () => {
   return (
     <div id="Header">
       <div class="Name">
-        <p>Sklep internetowy z ciuhami Hubiton</p>
+        <p>Sklep internetowy z ciuhami (to do - nazwa)</p>
       </div>
     </div>
   );
